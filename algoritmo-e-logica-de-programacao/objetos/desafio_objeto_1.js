b8d15366945802e06084155c61b18295b3a116e9
Anderson Bispo <andersonsbispo1997@outlook.com>
2023-04-02 15:04:39 -0300

    first commit

---
+++ algoritmo-e-logica-de-programacao/objetos/desafio_objeto_1.js
@@ -0,0 +1,15 @@
+// data
+// atributos: dia, mes e ano
+// método: exibir -> "dia/mes/ano"
+
+const data = {
+    dia: 22,
+    mes: 02,
+    ano: 2023,
+
+    exibir: function() {
+        return `${this.dia}/${this.mes}/${this.ano}`;
+    } 
+}
+
+console.log(data.exibir());
